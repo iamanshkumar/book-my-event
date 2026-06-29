f14ca27ab99d6ff454eda5b01a7bdee6f8758dea
Ansh Kumar <anshagroia@gmail.com>
2026-06-29 15:12:36 +0530

    (feat) : added feature for fetching all the active events

---
+++ src/app/api/events/list/route.ts
@@ -0,0 +1,47 @@
+import { prisma } from "@/backend/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    try{
+        const currentDateTime = new Date();
+
+        const events = await prisma.event.findMany({
+            where : {
+                status : "PUBLISHED",
+                dateTime : {
+                    gte : currentDateTime
+                },
+            },
+            include : {
+                ticketTiers : {
+                    select : {
+                        id : true,
+                        tierName : true,
+                        availableSeats : true,
+                        pricePerSeatExculdingTax  : true,
+                        taxPercentage : true
+                    }
+                },
+                organizer : {
+                    select : {
+                        name : true
+                    }
+                }
+            },
+            orderBy :{
+                dateTime : 'asc'
+            }
+        })
+
+        return NextResponse.json({
+            events
+        },{
+            status : 200
+        })
+    }catch(err : any){
+        return NextResponse.json(
+            { error: err.message || 'Failed to retrieve events.' },
+            { status: 500 }
+        );
+    }
+}
