c0cd8aa6aa24e7bb380cb9cfa31d971a86681e13
Ansh Kumar <anshagroia@gmail.com>
2026-06-30 22:34:20 +0530

    feat(admin) : added functionality for admin to see whole revenue of the platform

---
+++ src/app/api/admin/dashboard/route.ts
@@ -0,0 +1,45 @@
+import { prisma } from "@/backend/lib/prisma";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+export const dynamic = 'force-dynamic';
+
+export async function GET(){
+    try{
+        const headerList = await headers();
+        if(headerList.get("x-user-role")!=="ADMIN"){
+            return NextResponse.json({
+                error : "Forbidden."
+            },{
+                status : 403
+            });
+        }
+
+        const [totalUsers , totalEvents , bookingsData] = await prisma.$transaction([
+            prisma.user.count(),
+            prisma.event.count(),
+            prisma.booking.findMany({
+                where: { paymentStatus: 'SUCCESS' },
+                select: { totalPricePaid: true, quantity: true }
+            })
+        ]);
+
+        const globalRevenue = bookingsData.reduce((sum, b) => sum + Number(b.totalPricePaid), 0);
+        const globalTicketsSold = bookingsData.reduce((sum, b) => sum + b.quantity, 0);
+
+        return NextResponse.json({
+            metrics: {
+              globalRevenue,
+              globalTicketsSold,
+              totalRegisteredUsers: totalUsers,
+              totalEventsHosted: totalEvents
+            }
+        }, { status: 200 });
+    }catch(err : any){
+        return NextResponse.json({
+            error : err.message
+        },{
+            status : 500
+        });
+    }
+}
