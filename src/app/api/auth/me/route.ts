721ca54c0c3cfada622d2894686313d7d586017c
Ansh Kumar <anshagroia@gmail.com>
2026-06-30 16:36:21 +0530

    feat(auth) : added me route for accessing user data

---
+++ src/app/api/auth/me/route.ts
@@ -0,0 +1,50 @@
+import { prisma } from "@/backend/lib/prisma";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    try{
+        const headerList = await headers();
+        const userIdStr = headerList.get("x-user-id");
+
+        if(!userIdStr){
+            return NextResponse.json({
+                error : "Unauthorised. No active session discovered"
+            },{
+                status : 411
+            });
+        }
+
+        const userId = parseInt(userIdStr , 10);
+        const user = await prisma.user.findUnique({
+            where : {id : userId},
+            select : {
+                id : true,
+                name : true,
+                email : true,
+                role : true,
+                createdAt : true
+            }
+        });
+
+        if(!user){
+            return NextResponse.json({
+                error : "User account not found"
+            },{
+                status : 404
+            })
+        }
+
+        return NextResponse.json({
+            user
+        },{
+            status : 200
+        })
+    }catch(err : any){
+        return NextResponse.json({
+            error : "Failed to retrieve profile metadata."
+        },{
+            status : 500
+        })
+    }
+}
