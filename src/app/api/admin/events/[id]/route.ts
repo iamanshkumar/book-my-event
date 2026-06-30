aa898fd0b3ca34b4a0f5372dae840d1252f6bee9
Ansh Kumar <anshagroia@gmail.com>
2026-06-30 22:48:56 +0530

    feat(admin) : allowing admin to delete an event

---
+++ src/app/api/admin/events/[id]/route.ts
@@ -0,0 +1,41 @@
+import { prisma } from "@/backend/lib/prisma";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+type RouteContext = { params: Promise<{ id: string }> };
+
+export async function DELETE(request : Request , context : RouteContext){
+    try{
+        const {id} = await context.params;
+        const targetEventId = parseInt(id , 10);
+
+        const headersList = await headers();
+        if (headersList.get('x-user-role') !== 'ADMIN') {
+            return NextResponse.json({ error: 'Forbidden' }, { status: 403 });
+        }
+
+        const event = await prisma.event.findUnique({
+            where : {id : targetEventId}
+        });
+
+        if(!event){
+            return NextResponse.json({
+                error : "Event doesn't exists"
+            },{
+                status : 404
+            });
+        }
+        
+        await prisma.event.delete({
+            where: { id: targetEventId }
+        });
+
+        return NextResponse.json({
+            message : 'Administrative force-removal completed successfully.'
+        },{
+            status : 200
+        });
+    }catch(err : any){
+        return NextResponse.json({ error: err.message }, { status: 500 });
+    }
+}
