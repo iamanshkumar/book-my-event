634f4c7ba51dcd968e163c1e7d4cd14ea73fb5ce
Ansh Kumar <anshagroia@gmail.com>
2026-06-30 21:41:03 +0530

    feat(booking) : added functionality of allowing management teams to audit every individual registration across all events they have published.

---
+++ src/app/api/organizer/bookings/route.ts
@@ -0,0 +1,42 @@
+import { prisma } from "@/backend/lib/prisma";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    try{
+        const headerList = await headers();
+        const organizerId = parseInt(headerList.get("x-user-id")||'0' , 10);
+        const userRole = headerList.get("x-user-role");
+
+        if(userRole!=='ORGANIZER'){
+            return NextResponse.json({
+                error : 'Access denied. Organizers only.'
+            },{
+                status : 403
+            });
+        }
+
+        const organizerBookings = await prisma.booking.findMany({
+            where : {
+                event : {organizerId}
+            },
+            include : {
+                event: { select: { eventName: true, dateTime: true } },
+                ticketTier: { select: { tierName: true } },
+                user: { select: { name: true, email: true } }
+            },
+            orderBy : { createdAt: 'desc' }
+        });
+
+        return NextResponse.json(
+            {bookings: organizerBookings }, 
+            { status: 200 }
+        );
+    }catch(err : any){
+        return NextResponse.json({
+            error : err.message
+        },{
+            status : 500
+        });
+    }
+}
